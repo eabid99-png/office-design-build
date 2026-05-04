e7a2b9d7e804b35b223ed92684fa575108212d44
eabid99-png <eabid99@gmail.com>
2026-05-04 14:08:13 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <style>{`
+.hero-slide {
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  animation: slide 16s infinite;
+}
+
+@keyframes slide {
+  0% { background-image: url('/projects/whittington/IMG_1.JPEG'); }
+  25% { background-image: url('/projects/whittington/IMG_2.JPEG'); }
+  50% { background-image: url('/projects/whittington/IMG_3.JPEG'); }
+  75% { background-image: url('/projects/whittington/IMG_4.JPEG'); }
+  100% { background-image: url('/projects/whittington/IMG_1.JPEG'); }
+}
+`}</style>
     </html>
   );
 }
